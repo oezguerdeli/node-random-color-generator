21f9d7784bcbb044a5e681667c3561fbd84488ae
Özgür Deli <oezguer.deli@gmail.com>
2025-08-21 18:52:11 +0200

    Bug fixing of wrong logic in if condition

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
 // Declaration of variables
-const inputHue = 'random';
-const inputLuminosity = 'random';
+const inputLuminosity = 'light';
 
 // Creating a random hex color variable
 let generatedColor = undefined;
@@ -12,16 +11,14 @@ let generatedColor = undefined;
 // No arguments, random color will be created and used
 if (argv[2] === undefined) {
   // Random color will be used with random hue and luminosity
-  generatedColor = randomColor({
-    luminosity: inputLuminosity,
-    hue: inputHue,
-  });
-} else if (argv.length > 2 && argv.length < 4) {
+  generatedColor = randomColor();
+} else if (argv.length === 3) {
+  console.log(argv[2]);
   generatedColor = randomColor({
     luminosity: inputLuminosity,
     hue: argv[2],
   });
-} else if (argv.length >= 4) {
+} else if (argv.length > 3) {
   generatedColor = randomColor({
     luminosity: argv[3],
     hue: argv[2],
