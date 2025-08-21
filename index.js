79f36c25c304cc8b27584217bcf846f2b128f8ed
Özgür Deli <oezguer.deli@gmail.com>
2025-08-21 06:44:33 +0200

    Add random color functionality to create random color in hex

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-import convert from 'color-convert';
+// import convert from 'color-convert';
 
-// Testing output
+// Getting random color
+var randomColor = getRandomColor();
+
 console.log('###############################');
 console.log('###############################');
 console.log('###############################');
 console.log('#####                     #####');
+console.log(`#####       ${randomColor}       #####`);
 console.log('#####                     #####');
-console.log('#####                     #####');
 console.log('###############################');
 console.log('###############################');
 console.log('###############################');
+
+// Function to create a random color in hex format for every different possibilities in hex
+function getRandomColor() {
+  var letters = '0123456789abcdef';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
