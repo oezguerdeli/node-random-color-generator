2bd1dccb3456456321d6f79652b979c39104a1b2
Özgür Deli <oezguer.deli@gmail.com>
2025-08-21 09:08:50 +0200

    Add package randomcolor

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-// import convert from 'color-convert';
+import randomColor from 'randomcolor';
 
-// Getting random color
-var randomColor = getRandomColor();
+// Declaration of variables
+const hue = 'blue';
+const luminosity = 'dark';
+
+// Creating a random hex color
+const color = randomColor();
 
 console.log('###############################');
 console.log('###############################');
 console.log('###############################');
 console.log('#####                     #####');
-console.log(`#####       ${randomColor}       #####`);
+console.log('#####       ${color}       #####');
 console.log('#####                     #####');
 console.log('###############################');
 console.log('###############################');
 console.log('###############################');
-
-// Function to create a random color in hex format for every different possibilities in hex
-function getRandomColor() {
-  var letters = '0123456789abcdef';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
